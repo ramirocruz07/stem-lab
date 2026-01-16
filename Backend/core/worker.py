19f3487cec6021f1914599c460df4d5e6a0b38dd
ramirocruz07 <rohankadiyal@gmail.com>
2026-01-16 22:28:17 +0530

    created finished process signal

--- Backend/core/worker.py
+++ Backend/core/worker.py
@@ -3,8 +3,10 @@ import sys
 import subprocess
 from PyQt6.QtCore import QThread,QObject, pyqtSignal, pyqtSlot, QRunnable
 
-class SplitterWorker(QThread,QObject):
-    def __init__(self, file, stems, quality, export_mp3, device, output_dir):
+class SplitterWorker(QThread):
+    finished = pyqtSignal()
+    progress_changed = pyqtSignal(int)
+    def __init__(self, file, stems, quality, export_mp3, device, output_dir, progress):
         super().__init__()
         self.file = file
         self.stems = stems
@@ -18,13 +20,35 @@ class SplitterWorker(QThread,QObject):
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         separator_path = os.path.join(base_dir, "core", "separator.py")
 
-        subprocess.run([
-            sys.executable,
-            separator_path,
-            self.file,
-            str(self.stems),
-            self.quality,
-            str(int(self.export_mp3)),
-            self.device,
-            self.output_dir,
-        ])
+        process = subprocess.Popen(
+            [
+                sys.executable,
+                separator_path,
+                self.file,
+                str(self.stems),
+                self.quality,
+                str(int(self.export_mp3)),
+                self.device,
+                self.output_dir
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+            bufsize=1
+        )
+
+        for line in process.stdout:
+            line = line.strip()
+            print(line)
+
+            # Parse progress percentage
+            if "%" in line:
+                try:
+                    percent = int(line.split("%")[0].split()[-1])
+                    self.progress_changed.emit(percent)
+                except:
+                    pass
+
+        process.wait()
+        self.progress_changed.emit(100)
+        self.finished.emit()
