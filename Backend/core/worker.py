7a049ba061f973c54245b208c3a6d06d1f749bb6
ramirocruz07 <rohankadiyal@gmail.com>
2026-01-16 21:43:40 +0530

    updated

--- Backend/core/worker.py
+++ Backend/core/worker.py
@@ -1,15 +1,18 @@
 import os
 import sys
 import subprocess
-from PyQt6.QtCore import QThread
+from PyQt6.QtCore import QThread,QObject, pyqtSignal, pyqtSlot, QRunnable
 
-class SplitterWorker(QThread):
-    def __init__(self, file, stems, quality, export_mp3):
+class SplitterWorker(QThread,QObject):
+    def __init__(self, file, stems, quality, export_mp3, device, output_dir):
         super().__init__()
         self.file = file
         self.stems = stems
         self.quality = quality
         self.export_mp3 = export_mp3
+        self.device = device
+        self.output_dir = output_dir
+    
 
     def run(self):
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,5 +24,7 @@ class SplitterWorker(QThread):
             self.file,
             str(self.stems),
             self.quality,
-            str(int(self.export_mp3))
+            str(int(self.export_mp3)),
+            self.device,
+            self.output_dir,
         ])
