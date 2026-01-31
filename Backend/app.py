0437ac6970a9de722ba8d6d3cf5d4666a59ba11d
ramirocruz07 <rohankadiyal@gmail.com>
2026-01-31 15:00:09 +0530

    import issue fix

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from ui.mainwindow import MainWindow
+from ui.main_window import MainWindow
 
 app = QApplication(sys.argv)
 window = MainWindow()
